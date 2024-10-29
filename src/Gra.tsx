c25afb1975cf6f20fe1853fe95fb61804390716e
kacperrr <59144349+0273574@users.noreply.github.com>
2024-10-29 20:27:55 +0100

    Update Gra.tsx

--- src/Gra.tsx
+++ src/Gra.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { AlertCircle, Book, Flag, Shield, TrendingUp } from 'lucide-react';
+import { AlertCircle, Book } from 'lucide-react';
 import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
 
 interface Opcja {
@@ -15,84 +15,85 @@ interface Wydarzenie {
 }
 
 const wydarzenia: Wydarzenie[] = [
-  {
-    nazwa: "Kocioł pod Smoleńskiem",
-    opis: "Niemieckie wojska, nacierające w kierunku Moskwy w ramach operacji Barbarossa, otoczyły siły radzieckie w rejonie Smoleńska. Mimo twardego oporu, Armia Czerwona została okrążona, co doprowadziło do ogromnych strat po stronie ZSRR.",
-    opcje: {
-      "Przebić się": { zmianaSil: -1, zmianaMorale: 2 },
-      "Bronić pozycji": { zmianaSil: -2, zmianaMorale: 1 },
-      "Poddać się": { zmianaSil: -3, zmianaMorale: 0 },
-      "Zrzuty zaopatrzenia": { zmianaSil: -1, zmianaMorale: 3, info: "Zrzuty zaopatrzenia pozwalają na dłuższą obronę i podtrzymanie morale okrążonych wojsk." },
-      "Ataki bombowe na linie niemieckie": { zmianaSil: -2, zmianaMorale: 4, info: "Ataki bombowe osłabiają niemieckie okrążenie, dając szansę na przebicie się." }
-    }
-  },
-  {
-    nazwa: "Kocioł pod Kijowem",
-    opis: "Jedna z największych i najkrwawszych operacji okrążających na froncie wschodnim. Niemcy zdołali otoczyć i zniszczyć główne siły radzieckie wokół Kijowa.",
-    opcje: {
-      "Kontratakować": { zmianaSil: -2, zmianaMorale: 3 },
-      "Ewakuować": { zmianaSil: -1, zmianaMorale: 1 },
-      "Utrzymać Kijów": { zmianaSil: -3, zmianaMorale: 2 },
-      "Wsparcie pancerne": { zmianaSil: -2, zmianaMorale: 4, info: "Użycie rezerw pancernych może pomóc w przebiciu się z okrążenia, ale wiąże się z ryzykiem ich utraty." },
-      "Zrzuty zaopatrzenia": { zmianaSil: -1, zmianaMorale: 2, info: "Zrzuty zaopatrzenia przedłużają obronę i dają czas na zorganizowanie kontrataku." }
-    }
-  },
-  {
-    nazwa: "Oblężenie Leningradu",
-    opis: "Leningrad (obecny Petersburg) został otoczony przez wojska niemieckie i fińskie. Mieszkańcy miasta przez prawie 900 dni cierpieli głód, zimno i bombardowania, broniąc się zaciekle.",
-    opcje: {
-      "Wzmocnić obronę": { zmianaSil: 0, zmianaMorale: 2 },
-      "Ewakuować cywilów": { zmianaSil: -1, zmianaMorale: 1 },
-      "Przerwać okrążenie": { zmianaSil: -2, zmianaMorale: 3 },
-      "Produkcja czołgów w mieście": { zmianaSil: -1, zmianaMorale: 4, info: "Fabryki Leningradu kontynuują produkcję czołgów, wzmacniając obronę miasta." },
-      "Zrzuty zaopatrzenia": { zmianaSil: -1, zmianaMorale: 3, info: "Zrzuty zaopatrzenia przez Jezioro Ładoga ('Droga Życia') pomagają przetrwać oblężenie." }
-    }
-  },
-  {
-    nazwa: "Bitwa na Łuku Kurskim",
-    opis: "Największa bitwa pancerna w historii. Niemcy planują w ramach operacji 'Cytadela' rozbić siły radzieckie, ale czy radzieckie przygotowania i obrona skutecznie zniweczą te plany?",
-    opcje: {
-      "Przygotować obronę": { zmianaSil: 0, zmianaMorale: 3 },
-      "Zaatakować pierwsi": { zmianaSil: -2, zmianaMorale: 4 },
-      "Wycofać się": { zmianaSil: -1, zmianaMorale: 1 },
-      "Skierowanie lotnictwa na niemieckie kolumny pancerne": { zmianaSil: -2, zmianaMorale: 5, info: "Ataki lotnicze na niemieckie czołgi znacząco osłabiają siłę uderzeniową wroga." },
-      "Koncentracja czołgów w kluczowym miejscu": { zmianaSil: -1, zmianaMorale: 4, info: "Skoncentrowanie sił pancernych pozwala na skuteczne odparcie niemieckiego natarcia." }
-    }
-  },
-  {
-    nazwa: "Oblężenie Stalingradu",
-    opis: "Stalingrad był areną jednej z najkrwawszych i najważniejszych bitew II wojny światowej. Niemcy, chcąc zdobyć miasto nad Wołgą, napotkali na zaciekły opór Armii Czerwonej.",
-    opcje: {
-      "Bronić miasta za wszelką cenę": { zmianaSil: -2, zmianaMorale: 4 },
-      "Kontratak": { zmianaSil: -3, zmianaMorale: 5 },
-      "Wycofać się za Wołgę": { zmianaSil: -1, zmianaMorale: 0 },
-      "Przerzut czołgów z rezerw": { zmianaSil: -2, zmianaMorale: 6, info: "Wzmocnienie obrony Stalingradu czołgami z rezerw znacząco zwiększa szanse na utrzymanie miasta." },
-      "Ataki bombowe na linie niemieckie": { zmianaSil: -1, zmianaMorale: 3, info: "Bombardowania osłabiają niemieckie natarcie i utrudniają dostawy zaopatrzenia dla wroga." }
-    }
-  },
-  {
-    nazwa: "Walki o Dyneburg",
-    opis: "Dyneburg w Łotwie był jednym z kluczowych punktów obrony radzieckiej na Dźwinie w czasie niemieckiej inwazji na ZSRR. Jeśli Niemcy szybko przełamią obronę, zdobywając miasto otworzy im to drogę na dalsze terytoria ZSRR.",
-    opcje: {
-      "Wzmocnić garnizon": { zmianaSil: -1, zmianaMorale: 2 },
-      "Wysadzić mosty": { zmianaSil: 0, zmianaMorale: 1 },
-      "Ewakuować miasto": { zmianaSil: -2, zmianaMorale: 0 },
-      "Kontruderzenie": { zmianaSil: -2, zmianaMorale: 3, info: "Szybkie kontruderzenie może zaskoczyć niemieckie siły i opóźnić ich postęp." },
-      "Mobilizacja cywilów": { zmianaSil: -1, zmianaMorale: 2, info: "Zaangażowanie cywilów w obronę miasta zwiększa szanse na jego utrzymanie, ale może prowadzić do większych strat." }
-    }
-  },
-  {
-    nazwa: "Obrona Dźwiny",
-    opis: "Rzeka Dźwina stanowi ważną linię obrony dla sił radzieckich podczas niemieckiej inwazji. Obrona tej naturalnej bariery ma opóźnić postępy wroga i umożliwić mobilizację większych sił Armii Czerwonej.",
-    opcje: {
-      "Zaminować przeprawy": { zmianaSil: 0, zmianaMorale: 2 },
-      "Kontratak": { zmianaSil: -2, zmianaMorale: 3 },
-      "Wycofać się na drugą linię obrony": { zmianaSil: -1, zmianaMorale: 1 },
-      "Wzmocnienie artylerii": { zmianaSil: -1, zmianaMorale: 3, info: "Skoncentrowanie artylerii wzdłuż rzeki może skutecznie powstrzymać niemieckie próby przeprawy." },
-      "Taktyka spalonej ziemi": { zmianaSil: -2, zmianaMorale: 2, info: "Zniszczenie infrastruktury i zasobów spowolni niemiecki postęp, ale odbije się na ludności cywilnej." }
-    }
-  }
+    {
+        nazwa: "Kocioł pod Smoleńskiem",
+        opis: "Niemieckie wojska, nacierające w kierunku Moskwy w ramach operacji Barbarossa, otoczyły siły radzieckie w rejonie Smoleńska. Mimo twardego oporu, Armia Czerwona została okrążona, co doprowadziło do ogromnych strat po stronie ZSRR.",
+        opcje: {
+          "Przebić się": { zmianaSil: -1, zmianaMorale: 2 },
+          "Bronić pozycji": { zmianaSil: -2, zmianaMorale: 1 },
+          "Poddać się": { zmianaSil: -3, zmianaMorale: 0 },
+          "Zrzuty zaopatrzenia": { zmianaSil: -1, zmianaMorale: 3, info: "Zrzuty zaopatrzenia pozwalają na dłuższą obronę i podtrzymanie morale okrążonych wojsk." },
+          "Ataki bombowe na linie niemieckie": { zmianaSil: -2, zmianaMorale: 4, info: "Ataki bombowe osłabiają niemieckie okrążenie, dając szansę na przebicie się." }
+        }
+      },
+      {
+        nazwa: "Kocioł pod Kijowem",
+        opis: "Jedna z największych i najkrwawszych operacji okrążających na froncie wschodnim. Niemcy zdołali otoczyć i zniszczyć główne siły radzieckie wokół Kijowa.",
+        opcje: {
+          "Kontratakować": { zmianaSil: -2, zmianaMorale: 3 },
+          "Ewakuować": { zmianaSil: -1, zmianaMorale: 1 },
+          "Utrzymać Kijów": { zmianaSil: -3, zmianaMorale: 2 },
+          "Wsparcie pancerne": { zmianaSil: -2, zmianaMorale: 4, info: "Użycie rezerw pancernych może pomóc w przebiciu się z okrążenia, ale wiąże się z ryzykiem ich utraty." },
+          "Zrzuty zaopatrzenia": { zmianaSil: -1, zmianaMorale: 2, info: "Zrzuty zaopatrzenia przedłużają obronę i dają czas na zorganizowanie kontrataku." }
+        }
+      },
+      {
+        nazwa: "Oblężenie Leningradu",
+        opis: "Leningrad (obecny Petersburg) został otoczony przez wojska niemieckie i fińskie. Mieszkańcy miasta przez prawie 900 dni cierpieli głód, zimno i bombardowania, broniąc się zaciekle.",
+        opcje: {
+          "Wzmocnić obronę": { zmianaSil: 0, zmianaMorale: 2 },
+          "Ewakuować cywilów": { zmianaSil: -1, zmianaMorale: 1 },
+          "Przerwać okrążenie": { zmianaSil: -2, zmianaMorale: 3 },
+          "Produkcja czołgów w mieście": { zmianaSil: -1, zmianaMorale: 4, info: "Fabryki Leningradu kontynuują produkcję czołgów, wzmacniając obronę miasta." },
+          "Zrzuty zaopatrzenia": { zmianaSil: -1, zmianaMorale: 3, info: "Zrzuty zaopatrzenia przez Jezioro Ładoga ('Droga Życia') pomagają przetrwać oblężenie." }
+        }
+      },
+      {
+        nazwa: "Bitwa na Łuku Kurskim",
+        opis: "Największa bitwa pancerna w historii. Niemcy planują w ramach operacji 'Cytadela' rozbić siły radzieckie, ale czy radzieckie przygotowania i obrona skutecznie zniweczą te plany?",
+        opcje: {
+          "Przygotować obronę": { zmianaSil: 0, zmianaMorale: 3 },
+          "Zaatakować pierwsi": { zmianaSil: -2, zmianaMorale: 4 },
+          "Wycofać się": { zmianaSil: -1, zmianaMorale: 1 },
+          "Skierowanie lotnictwa na niemieckie kolumny pancerne": { zmianaSil: -2, zmianaMorale: 5, info: "Ataki lotnicze na niemieckie czołgi znacząco osłabiają siłę uderzeniową wroga." },
+          "Koncentracja czołgów w kluczowym miejscu": { zmianaSil: -1, zmianaMorale: 4, info: "Skoncentrowanie sił pancernych pozwala na skuteczne odparcie niemieckiego natarcia." }
+        }
+      },
+      {
+        nazwa: "Oblężenie Stalingradu",
+        opis: "Stalingrad był areną jednej z najkrwawszych i najważniejszych bitew II wojny światowej. Niemcy, chcąc zdobyć miasto nad Wołgą, napotkali na zaciekły opór Armii Czerwonej.",
+        opcje: {
+          "Bronić miasta za wszelką cenę": { zmianaSil: -2, zmianaMorale: 4 },
+          "Kontratak": { zmianaSil: -3, zmianaMorale: 5 },
+          "Wycofać się za Wołgę": { zmianaSil: -1, zmianaMorale: 0 },
+          "Przerzut czołgów z rezerw": { zmianaSil: -2, zmianaMorale: 6, info: "Wzmocnienie obrony Stalingradu czołgami z rezerw znacząco zwiększa szanse na utrzymanie miasta." },
+          "Ataki bombowe na linie niemieckie": { zmianaSil: -1, zmianaMorale: 3, info: "Bombardowania osłabiają niemieckie natarcie i utrudniają dostawy zaopatrzenia dla wroga." }
+        }
+      },
+      {
+        nazwa: "Walki o Dyneburg",
+        opis: "Dyneburg w Łotwie był jednym z kluczowych punktów obrony radzieckiej na Dźwinie w czasie niemieckiej inwazji na ZSRR. Jeśli Niemcy szybko przełamią obronę, zdobywając miasto otworzy im to drogę na dalsze terytoria ZSRR.",
+        opcje: {
+          "Wzmocnić garnizon": { zmianaSil: -1, zmianaMorale: 2 },
+          "Wysadzić mosty": { zmianaSil: 0, zmianaMorale: 1 },
+          "Ewakuować miasto": { zmianaSil: -2, zmianaMorale: 0 },
+          "Kontruderzenie": { zmianaSil: -2, zmianaMorale: 3, info: "Szybkie kontruderzenie może zaskoczyć niemieckie siły i opóźnić ich postęp." },
+          "Mobilizacja cywilów": { zmianaSil: -1, zmianaMorale: 2, info: "Zaangażowanie cywilów w obronę miasta zwiększa szanse na jego utrzymanie, ale może prowadzić do większych strat." }
+        }
+      },
+      {
+        nazwa: "Obrona Dźwiny",
+        opis: "Rzeka Dźwina stanowi ważną linię obrony dla sił radzieckich podczas niemieckiej inwazji. Obrona tej naturalnej bariery ma opóźnić postępy wroga i umożliwić mobilizację większych sił Armii Czerwonej.",
+        opcje: {
+          "Zaminować przeprawy": { zmianaSil: 0, zmianaMorale: 2 },
+          "Kontratak": { zmianaSil: -2, zmianaMorale: 3 },
+          "Wycofać się na drugą linię obrony": { zmianaSil: -1, zmianaMorale: 1 },
+          "Wzmocnienie artylerii": { zmianaSil: -1, zmianaMorale: 3, info: "Skoncentrowanie artylerii wzdłuż rzeki może skutecznie powstrzymać niemieckie próby przeprawy." },
+          "Taktyka spalonej ziemi": { zmianaSil: -2, zmianaMorale: 2, info: "Zniszczenie infrastruktury i zasobów spowolni niemiecki postęp, ale odbije się na ludności cywilnej." }
+        }
+      }
 ];
+
 interface AlertProps {
   title: string;
   children: React.ReactNode;
@@ -110,6 +111,7 @@ const Alert: React.FC<AlertProps> = ({ title, children, icon }) => (
 );
 
 const GraFrontWschodni: React.FC = () => {
+  const [mode, setMode] = useState<'start' | 'game' | 'info'>('start');
   const [punkty, setPunkty] = useState(10);
   const [morale, setMorale] = useState(5);
   const [tura, setTura] = useState(1);
@@ -122,13 +124,13 @@ const GraFrontWschodni: React.FC = () => {
   ]);
 
   useEffect(() => {
-    if (tura <= 10 && punkty > 0 && morale > 0) {
+    if (mode === 'game' && tura <= 10 && punkty > 0 && morale > 0) {
       setWydarzenie(wydarzenia[Math.floor(Math.random() * wydarzenia.length)]);
       setStatystyki(prev => [...prev, { tura, punkty, morale }]);
-    } else {
+    } else if (mode === 'game') {
       zakonczGre();
     }
-  }, [tura, punkty, morale]);
+  }, [tura, punkty, morale, mode]);
 
   const wykonajAkcje = (zmianaSil: number, zmianaMorale: number, info: string = "") => {
     setPunkty((prev) => Math.max(0, prev + zmianaSil));
@@ -155,6 +157,54 @@ const GraFrontWschodni: React.FC = () => {
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-red-700 via-red-500 to-yellow-500 p-8">
+      <div className="max-w-4xl mx-auto bg-white/90 p-8 rounded-2xl shadow-2xl">
+        <h1 className="text-5xl font-bold mb-8 text-center text-red-800">Front Wschodni 1941</h1>
+
+        {mode === 'start' && (
+          <div className="text-center">
+            <button
+              onClick={() => setMode('game')}
+              className="bg-green-500 hover:bg-green-700 text-white font-bold py-3 px-6 rounded-lg mb-4"
+            >
+              Zagraj w grę
+            </button>
+            <button
+              onClick={() => setMode('info')}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-lg"
+            >
+              Informacje historyczne
+            </button>
+          </div>
+        )}
+
+        {mode === 'info' && (
+          <div>
+            {wydarzenia.map((event, index) => (
+              <div key={index} className="mb-8 bg-blue-50 p-6 rounded-lg shadow">
+                <h2 className="text-2xl font-semibold mb-4 text-blue-800">{event.nazwa}</h2>
+                <p className="mb-6 text-gray-700">{event.opis}</p>
+                <ul>
+                  {Object.entries(event.opcje).map(([opcja, { zmianaSil, zmianaMorale, info }]) => (
+                    <li key={opcja} className="text-gray-600 mb-2">
+                      <strong>{opcja}:</strong> Zmiana sił: {zmianaSil}, Zmiana morale: {zmianaMorale}
+                      {info && <p className="text-gray-500 text-sm">Info: {info}</p>}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+            <button
+              onClick={() => setMode('start')}
+              className="bg-red-500 hover:bg-red-700 text-white font-bold py-3 px-6 rounded-lg mt-8 block mx-auto"
+            >
+              Powrót
+            </button>
+          </div>
+        )}
+
+        {mode === 'game' && (
+          <>
+            {<div className="min-h-screen bg-gradient-to-br from-red-700 via-red-500 to-yellow-500 p-8">
       <div className="max-w-4xl mx-auto bg-white/90 p-8 rounded-2xl shadow-2xl">
         <h1 className="text-5xl font-bold mb-8 text-center text-red-800">Front Wschodni 1941</h1>
         
@@ -242,6 +292,10 @@ const GraFrontWschodni: React.FC = () => {
               </button>
             </div>
           </div>
+        )}
+      </div>
+    </div>}
+          </>
         )}
       </div>
     </div>
